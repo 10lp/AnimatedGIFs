179a2d6593f073beb29cbaf9bffe06a3f91b95ab
Marc MERLIN <marc_soft@merlins.org>
2021-01-29 20:21:53 -0800

    Use less RAM on ESP8266 with lzwMaxBits 11

--- animatedgif_config.h
+++ animatedgif_config.h
@@ -6,7 +6,11 @@
  * lzwMaxBits can be set to 10 or 11 for small displays, 12 for large displays
  * All 32x32-pixel GIFs tested work with 11, most work with 10
  */
+#ifdef ESP8266
+const int lzwMaxBits = 11;
+#else
 const int lzwMaxBits = 12;
+#endif
 
 //#define NEOMATRIX // Switch to NEOMATRIX backend from native SMARTMATRIX backend
 //#define NEOPIXEL_MATRIX  // If NEOMATRIX, use FastLED::NeoMatrix, not SmartMatrix_GFX
